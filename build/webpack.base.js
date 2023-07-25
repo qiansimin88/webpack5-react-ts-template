75f0a825586faadb8ee7c2c8ef20ce9e2953af8c
qsm <348867341@qq.com>
2023-07-26 04:25:47 +0800

    init

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -2,7 +2,7 @@
  * @Author: qsm 348867341@qq.com
  * @Date: 2023-07-25 03:46:51
  * @LastEditors: qsm 348867341@qq.com
- * @LastEditTime: 2023-07-25 05:44:38
+ * @LastEditTime: 2023-07-26 03:46:37
  * @FilePath: /webpack5-react-ts-template/build/webpack.base.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,7 +11,10 @@
 // 基础配置
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin') // 打包注入到html
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 打包环境抽离CSS 方便缓存 开发环境则不要 开发环境style-loader嵌入head内
+
+const isDev = process.env.NODE_ENV === 'development' // 是否是开发模式
 const chalk = require( 'chalk' )
 
 const g = chalk.green.bold
@@ -26,7 +29,7 @@ module.exports = {
     entry: path.join(__dirname, '../src/index.tsx'), 
     // 打包文件出口
     output: {
-        filename: 'static/js/[name].js', // 每个输出js的路径名称
+        filename: 'static/js/[name].[chunkhash:8].js', // 每个输出js的路径名称 加上8位hash利用缓存
         path: path.join(__dirname, '../dist'), // 打包结果输出路径
         clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
         publicPath: '/' // 打包后文件的公共前缀路径
@@ -35,8 +38,10 @@ module.exports = {
     module: {
         rules: [
           {
+              include: [path.resolve(__dirname, '../src')], // 只对项目src文件的ts,tsx进行loader解析
               test: /.(ts|tsx)$/,
-              use: 'babel-loader'
+              // thread-loader 在这个loader之后的loader都新开web worker中心运行 多线程
+              use: ['thread-loader', 'babel-loader']
               // webpack自动引入babel.config.js中的配置
               // options: {
               //    // 执行顺序由右往左,所以先处理ts,再处理jsx,最后再试一下babel转换为低版本语法
@@ -58,13 +63,34 @@ module.exports = {
               //   ]
               // }
           },
-          {
-            test: /.(css|less)$/, //匹配 css less 文件 执行顺序是从右往左 从下往上
+          // {
+          //   include: [path.resolve(__dirname, '../src')], 
+          //   test: /.(css|less)$/, 
+          //   use: [
+          //     'style-loader',
+          //     'css-loader',
+          //     'postcss-loader',
+          //     'less-loader'
+          //   ]
+          // },
+            //匹配 css less 文件 执行顺序是从右往左 从下往上
             // style-loader: 把解析后的css代码从js中抽离,放到头部的style标签中(在运行时做的)
             // css-loader: 解析css文件代码
             // 'less-loader' less解析成CSS
+          {
+            test: /.css$/, //匹配所有的 css 文件
+            include: [path.resolve(__dirname, '../src')],
             use: [
-              'style-loader',
+              isDev ? 'style-loader' : MiniCssExtractPlugin.loader, // 开发环境使用style-looader,打包模式抽离css
+              'css-loader',
+              'postcss-loader'
+            ]
+          },
+          {
+            test: /.less$/, //匹配所有的 less 文件
+            include: [path.resolve(__dirname, '../src')],
+            use: [
+              isDev ? 'style-loader' : MiniCssExtractPlugin.loader, // 开发环境使用style-looader,打包模式抽离css
               'css-loader',
               'postcss-loader',
               'less-loader'
@@ -79,7 +105,7 @@ module.exports = {
               }
             },
             generator:{ 
-              filename:'static/images/[name][ext]', // 文件输出目录和命名
+              filename:'static/images/[name].[contenthash:8][ext]', // 文件输出目录和命名
             },
           },
           {
@@ -91,7 +117,7 @@ module.exports = {
               }
             },
             generator:{ 
-              filename:'static/fonts/[name][ext]', // 文件输出目录和命名
+              filename:'static/fonts/[name].[contenthash:8][ext]', // 文件输出目录和命名
             },
           },
           {
@@ -103,15 +129,33 @@ module.exports = {
               }
             },
             generator:{ 
-              filename:'static/media/[name][ext]', // 文件输出目录和命名
+              filename:'static/media/[name].[contenthash:8][ext]', // 文件输出目录和命名
             },
           },
         ]
     },
+    // 解析
     resolve: {
         // 识别不带后缀的文件
         extensions: ['.js', '.tsx', '.ts'],
+         // 别名 方便项目引用
+        alias: {
+          '@': path.join(__dirname, '../src')
+        },
     },
+    // 开启缓存 提速90%
+    cache: {
+      type: 'filesystem', // 使用文件缓存
+    },
+    // inline 代码内通过dataUrl 形式引入SourceMap
+    // hidden 生成 SourceMap 文件,但不使用
+    // eval eval(...) 形式执行代码,通过 dataUrl 形式引入 SourceMap
+    // nosources 不生成 SourceMap
+    // cheap 只需要定位到行信息,不需要列信息
+    // module 展示源代码中的错误位置
+    // source map配置 
+    // devtool的命名规则为 ^(inline-|hidden-|eval-)?(nosources-)?(cheap-(module-)?)?source-map$
+    devtool: 'eval-cheap-module-source-map',
     // 插件
     plugins: [
         // webpack需要把最终构建好的静态资源都引入到一个html文件中
--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@
  * @Author: qsm 348867341@qq.com
  * @Date: 2023-07-25 03:47:58
  * @LastEditors: qsm 348867341@qq.com
- * @LastEditTime: 2023-07-25 05:50:19
+ * @LastEditTime: 2023-07-26 03:03:43
  * @FilePath: /webpack5-react-ts-template/src/App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React from 'react'
-import './app.less'
+import '@/app.less'
 import meinv from '../public/meinv.webp'
 
 function App() {
   return <div>
-      <h2>webpack5-r22222211111eact-ts113322221131</h2>
+      <h2>webpack5-r22222211111eact-2222211111122222222211111122ts113322221131</h2>
       {/* <img src="/public/result@2.png" alt="" /> */}
       <img src={meinv} alt="" />
   </div>
